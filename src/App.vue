fbf5e5dc88421bb3dd53b5a64f0dfa58bc625d98
Joel Ccasane <joelccasane@gmail.com>
2024-10-22 11:24:19 -0500

    modify reset styles

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,7 @@
     <TaskProView></TaskProView>
 </template>
 <script setup lang="ts">
+import '@fontsource-variable/montserrat';
+import '@fontsource-variable/inter';
 import TaskProView from '@/view/TaskProView.vue';
 </script>
--- src/view/TaskProView.vue
+++ src/view/TaskProView.vue
@@ -8,9 +8,9 @@
 </template>
 <script setup lang="ts">
 import HeaderApp from '@/components/HeaderApp.vue';
-import FooterApp from '@/components/FooterApp.vue';
 import HeroSection from '@/sections/HeroSection.vue';
 import BenefitSection from '@/sections/BenefitSection.vue';
 import TestimonialSection from '@/sections/TestimonialSection.vue';
 import StoreSection from '@/sections/StoreSection.vue';
+import FooterApp from '@/components/FooterApp.vue';
 </script>
