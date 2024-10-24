dc992e79adaa193aa853be8592d957a750899eae
Joel Ccasane <joelccasane@gmail.com>
2024-10-23 19:56:29 -0500

    modify reset styles

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@import "@/assets/styles/base.scss";`,
+                additionalData: `@import "@/assets/styles/_variables.scss";`,
             },
         },
     },
