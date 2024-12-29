e3c0d93d49bfa6f1a463f58df36cef71c929c135
Joel Ccasane <joelccasane@gmail.com>
2024-12-28 23:19:44 -0500

    update footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,20 +1,16 @@
 <template>
-    <footer class="footer">
-        <div class="footer__container">
-            <p>
-                © {{ dateFooter }} <a href="javascript:void(0)">TaskPro</a>.
-                Todos los derechos reservados.
-            </p>
-            <div class="footer__links">
-                <a href="javascript:void(0)" aria-label="Terms and Conditions"
-                    >Términos y Condiciones</a
-                >
-                <a href="javascript:void(0)" aria-label="Privacy Policy"
-                    >Política de Privacidad</a
-                >
-            </div>
-        </div>
-    </footer>
+  <footer class="footer">
+    <div class="footer__container">
+      <p>
+        © {{ dateFooter }} <a href="javascript:void(0)" aria-label="TaskPro">TaskPro</a>. Todos los
+        derechos reservados.
+      </p>
+      <div class="footer__links">
+        <a href="javascript:void(0)" aria-label="Términos y Condiciones">Términos y Condiciones</a>
+        <a href="javascript:void(0)" aria-label="Política de Privacidad">Política de Privacidad</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -24,48 +20,48 @@ const dateFooter = ref(new Date().getFullYear().toString());
 
 <style scoped lang="scss">
 .footer {
-    background-color: $primary-color;
-    padding: 2rem 1rem;
+  background-color: $primary-color;
+  padding: 2.5rem 1rem;
 
-    &__container {
-        @include containerWidth;
-        @include flexBetween;
+  &__container {
+    @include containerWidth;
+    @include flexBetween;
 
-        p {
-            color: $neutral-gray;
-            font-size: 0.875rem;
-        }
-
-        a {
-            color: $neutral-gray;
-            transition: color 0.29s ease;
-
-            &:hover {
-                color: $background-light;
-            }
-        }
+    p {
+      color: $neutral-gray;
+      font-size: 0.875rem;
     }
 
-    &__links {
-        display: flex;
-        gap: 1.2rem;
+    a {
+      color: $neutral-gray;
+      transition: color 0.29s ease;
 
-        a {
-            font-size: 0.875rem;
-            color: $neutral-gray;
-            transition: color 0.29s ease;
-
-            &:hover {
-                color: $background-light;
-            }
-        }
+      &:hover {
+        color: $background-light;
+      }
     }
+  }
+
+  &__links {
+    display: flex;
+    gap: 1.2rem;
+
+    a {
+      font-size: 0.875rem;
+      color: $neutral-gray;
+      transition: color 0.29s ease;
+
+      &:hover {
+        color: $background-light;
+      }
+    }
+  }
 }
 
 @media (max-width: 768px) {
-    .footer__container {
-        flex-direction: column;
-        gap: 0.4rem;
-    }
+  .footer__container {
+    flex-direction: column;
+    gap: 0.4rem;
+  }
 }
 </style>
