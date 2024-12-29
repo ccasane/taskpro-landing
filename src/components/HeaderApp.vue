7f69d5fef81acfe3eabddba93437be3423ee1197
Joel Ccasane <joelccasane@gmail.com>
2024-12-28 21:45:33 -0500

    update header

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,27 +1,27 @@
 <template>
-    <header class="header">
-        <div class="header__container">
-            <a href="/">TaskPro</a>
-        </div>
-    </header>
+  <header class="header">
+    <div class="header__container">
+      <a href="/">TaskPro</a>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
 .header {
-    background-color: $primary-color;
-    padding: 0.6rem 1rem;
+  background-color: $primary-color;
+  padding: 0.8rem 1rem;
 
-    &__container {
-        @include containerWidth;
-        @include flexCenter;
-    }
+  &__container {
+    @include containerWidth;
+    @include flexCenter;
+  }
 
-    a {
-        font-size: 2rem;
-        font-weight: 700;
-        color: $background-light;
-    }
+  a {
+    font-size: 2.5rem;
+    font-weight: 700;
+    color: $background-light;
+  }
 }
 </style>
